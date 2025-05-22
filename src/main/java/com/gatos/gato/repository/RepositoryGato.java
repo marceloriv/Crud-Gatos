924ae5d780a755d0e3f4a8ffada2270f179fc621
marceloriv <ma.riveracontreras@duocuc.com>
2025-05-22 15:25:38 -0400

    Agregar método para crear un gato en RepositoryGato

--- src/main/java/com/gatos/gato/repository/RepositoryGato.java
+++ src/main/java/com/gatos/gato/repository/RepositoryGato.java
@@ -1,6 +1,7 @@
 package com.gatos.gato.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gatos.gato.model.GatoModel;
@@ -12,4 +13,7 @@ public interface RepositoryGato extends JpaRepository<GatoModel, Long> {
 
     // Método para buscar un gato por su nombre
     GatoModel findByNombre(String nombre);
+
+    @Query("INSERT INTO GatoModel (raza, nombre, edad, color, sexo) VALUES (:raza, :nombre, :edad, :color, :sexo)")
+    GatoModel crearGato(String raza, String nombre, String edad, String color, String sexo);
 }
