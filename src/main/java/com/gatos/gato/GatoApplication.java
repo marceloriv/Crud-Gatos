02fa6ea05e0e577c83ec17ea3387acdd7a6cabed
marceloriv <ma.riveracontreras@duocuc.com>
2025-05-21 23:04:21 -0400

    Inicializar Proyecto

---
+++ src/main/java/com/gatos/gato/GatoApplication.java
@@ -0,0 +1,13 @@
+package com.gatos.gato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gatos/gato/GatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatos.gato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
